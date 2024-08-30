ba41f2344663efd7ce5d34c843c5dc0d46c09af7
Marcap00 <cappellatomarco@yahoo.com>
2024-08-30 18:22:40 +0200

    add some ways to completed snack 3

--- js/snack3.js
+++ js/snack3.js
@@ -33,27 +33,34 @@ const roadBikes = [
 const [firstBike] = roadBikes;
 // # Fase di elaborazione
 
-// $ METODO FILTER
+/* // $ METODO FILTER
 // Utilizziamo il metodo filter per creare un array filtrando quella con il peso minore
 // Dichiaro il primo oggetto dell'array (la "prima" bike) la più leggera
 
-// let lightest = firstBike;
+let lightest = firstBike;
 
 // Faccio il confronto con filter
 
-// const lightestBike = roadBikes.filter(bike => lightest = lightest.weight < bike.weight ? lightest : bike);
-// console.log(lightest);
+const lightestBike = roadBikes.filter(bike => lightest = lightest.weight < bike.weight ? lightest : bike);
+console.log(lightest); */
+
+/* // $ METODO MAP
+// Utilizziamo il metodo map
+let lightest = firstBike;
+const lightestBike = roadBikes.map(bike => lightest = lightest.weight < bike.weight ? lightest : bike);
+
+console.log(lightest); */
 
 // $ METODO REDUCE
 // Utilizziamo il metodo reduce 
-const lightestBike = roadBikes.reduce((lightest, bike) => {
-    return lightest = lightest.weight < bike.weight ? lightest : bike;
-}, firstBike)
+const lightestBike = roadBikes.reduce((lightest, bike) => lightest = lightest.weight < bike.weight ? lightest : bike, firstBike);
 
 console.table(lightestBike);
 
 // # Fase di output
-// Destrutturo l'oggetto lightest
+// $ Destrutturo l'oggetto lightest quando uso map o filter
+/* const { name, weight } = lightest; */
+// $ Destrutturo l'oggetto lightest quando uso reduce
 const { name, weight } = lightestBike;
 console.log(`
     La bicicletta da corsa più leggera è:
